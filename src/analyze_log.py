1efb7ca6aea430c343a7e0f6fc98bf913729ca23
Joiscilene <joisci.lene@hotmail.com>
2022-12-26 22:47:31 -0300

    Adicionando requisitos 1 e 2

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,3 +1,133 @@
+import csv
+
+
+every_product = ["hamburguer", "coxinha", "misto-quente", "pizza"]
+days = ["sabado", "terça-feira", "segunda-feira"]
+
+def get_data(path_file):
+    if ".csv" not in path_file:
+        raise FileNotFoundError(f"Extensão inválida. {path_file}")
+    try:
+        with open(path_file, encoding="utf8") as file:
+            csv_reader = csv.reader(file, delimiter=",", quotechar='"')
+            header, *data = csv_reader
+            return data
+
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Arquivo inexistente: {path_file}")
+
+
+def get_list(list):
+    repeated_names = []
+    person_list = []
+    for row in list:
+        if row[0] not in repeated_names:
+            repeated_names.append(row[0])
+            person_list.append(
+                {
+                    row[0]: {
+                        "pedidos": [
+                            {
+                                "produto": row[1],
+                                "dia": row[2],
+                            }
+                        ]
+                    }
+                }
+            )
+        for person in person_list:
+            keys = person.keys()
+            if row[0] in keys:
+                person[row[0]]["pedidos"].append(
+                    {
+                        "produto": row[1],
+                        "dia": row[2],
+                    }
+                )
+    return person_list
+
+def get_food_count_by_person(person, list):
+    for row in list:
+        keys = row.keys()
+        if person in keys:
+            return row
+
+def most_requested_food_by_maria(list):
+    maria_food_count = get_food_count_by_person("maria", list)
+    products_list = []
+
+    for row in maria_food_count["maria"]["pedidos"]:
+        products_list.append(row["produto"])
+
+    counter = 0
+    element = products_list[0]
+
+    for maria in products_list:
+        maria_most_ordered_food = products_list.count(maria)
+        if maria_most_ordered_food > counter:
+            counter = maria_most_ordered_food
+            element = maria
+
+    return element
+
+
+def count_requested_hamburger_by_arnaldo(list):
+    arnaldo_food_count = get_food_count_by_person("arnaldo", list)
+    products_list = []
+
+    for row in arnaldo_food_count["arnaldo"]["pedidos"]:
+        products_list.append(row["produto"])
+
+    counter = 0
+
+    for arnaldo in products_list:
+        if arnaldo == 'hamburguer':
+            counter += 1
+
+    return counter
+
+
+def food_joao_never_requested(list):
+    joao_food_count = get_food_count_by_person("joao", list)
+    products_list = []
+
+    for row in joao_food_count["joao"]["pedidos"]:
+        products_list.append(row["produto"])
+
+    never_asked = set()
+
+    for joao in every_product:
+        if joao not in products_list:
+            never_asked.add(joao)
+
+    return never_asked
+
+
+def days_that_joao_never_went(list):
+    joao_food_days = get_food_count_by_person("joao", list)
+    days_list = []
+
+    for row in joao_food_days["joao"]["pedidos"]:
+        days_list.append(row["dia"])
+
+    never_asked = set()
+
+    for joao_days in days:
+        if joao_days not in days_list:
+            never_asked.add(joao_days)
+
+    return never_asked
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
-# iniciando
+   # raise NotImplementedError
+    csv_list = get_data(path_to_file)
+    person_list = get_list(csv_list)
+    maria = most_requested_food_by_maria(person_list)
+    arnaldo = count_requested_hamburger_by_arnaldo(person_list)
+    joao = food_joao_never_requested(person_list)
+    joao_days = days_that_joao_never_went(person_list)
+
+    file = open("data/mkt_campaign.txt", mode="w")
+    LINES = [f"{maria}\n", f"{arnaldo}\n", f"{joao}\n", f"{joao_days}\n"]
+    file.writelines(LINES)
+    file.close()            
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,22 +1,90 @@
+from collections import Counter
+
+
 class TrackOrders:
     # aqui deve expor a quantidade de estoque
+    def __init__(self):
+        self.lista_pedidos = []
+        pass
+
     def __len__(self):
         pass
+        return len(self.lista_pedidos)
 
     def add_new_order(self, customer, order, day):
         pass
+        self.lista_pedidos.append({
+            'customer': customer,
+            'order': order,
+            'day': day
+        })
+        return self.lista_pedidos    
 
     def get_most_ordered_dish_per_customer(self, customer):
         pass
+        customer_list = []
+        for pedidos in self.lista_pedidos:
+            if pedidos['customer'] == customer:
+                customer_list.append(pedidos['order'])
+
+        orders_statistics = Counter(customer_list)
+
+        mais_pedida = orders_statistics.most_common()
+
+        return mais_pedida[0][0]     
 
     def get_never_ordered_per_customer(self, customer):
         pass
+        customer_list = []
+        all_orders = []
+
+        for pedidos in self.lista_pedidos:
+            all_orders.append(pedidos['order'])
+
+            if pedidos['customer'] == customer:
+                customer_list.append(pedidos['order'])
+
+        all_unique_orders = set(all_orders)
+        customer_unique_orders = set(customer_list)
+
+        return all_unique_orders.difference(customer_unique_orders)        
 
     def get_days_never_visited_per_customer(self, customer):
         pass
+        customer_days = []
+        all_days = []
+
+        for pedidos in self.lista_pedidos:
+            all_days.append(pedidos['day'])
+
+            if pedidos['customer'] == customer:
+                customer_days.append(pedidos['day'])
+
+        all_unique_days = set(all_days)
+        customer_unique_days = set(customer_days)
+
+        return all_unique_days.difference(customer_unique_days)
+
+    def get_all_days_list(self):
+        all_days = []
+        for pedidos in self.lista_pedidos:
+            all_days.append(pedidos['day'])
+
+        return all_days        
 
     def get_busiest_day(self):
         pass
+        all_days = self.get_all_days_list()
+        all_unique_days = Counter(all_days)
+        days_statistics = all_unique_days.most_common()
+
+        return days_statistics[0][0]
+        
 
     def get_least_busy_day(self):
         pass
+        all_days = self.get_all_days_list()
+        all_unique_days = Counter(all_days)
+        days_statistics = all_unique_days.most_common()
+
+        return days_statistics[-1][0]
